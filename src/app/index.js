1e93e2e559f9122c3eff42ade071a899180b3b62
Greg Pascale <gregpascale@Gregs-Mac-mini.local>
2016-10-08 09:58:41 -0700

    tweak

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/App';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
+import reducers from './reducers';
+
+import './components/bundle.scss';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+import teoria from 'teoria'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory}>
+      <Route path="/" component={App}>
+	<IndexRoute component={Home} />;
+	<Route path="/about" component={About} />
+      </Route>
+    </Router>
+  </Provider>
+  , document.getElementById('react-root'));
+
+// test
+var a4 = teoria.Note('a4');
