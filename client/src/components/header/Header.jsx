c15ea4d7157c950166af844b4f59aabe4ca2e0a0
Greg Pascale <greg.pascale@gmail.com>
2016-11-23 16:40:34 -0800

    Add left-hand drawer

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 export default class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerOpen: false
+    };
+  }
+
   render() {
     return (
-      <AppBar title="Jazzercise"/>
+      <AppBar title="Jazzercise"
+              onLeftIconButtonTouchTap={() => this.setState({drawerOpen: true})}>
+        <Drawer docked={false}
+                width={250}
+                open={this.state.drawerOpen}
+                onRequestChange={(open) => {
+                  this.setState({drawerOpen: open})}
+                }>
+          <AppBar className="drawerHeader" title="Jazzercise"
+                  iconElementLeft={(<div></div>) /* no left element */}/>
+          <MenuItem className="drawerItem">Create A New Exercise</MenuItem>
+          <MenuItem className="drawerItem">Load an Existing Exercise</MenuItem>
+        </Drawer>
+      </AppBar>
     );
   }
 }
