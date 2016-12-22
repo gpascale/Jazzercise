ed8ce50f03a9c037a5fc2200456d336393b60944
Greg Pascale <greg.pascale@gmail.com>
2016-12-22 02:58:53 +0000

    run on port 3000

--- client/server.js
+++ client/server.js
@@ -8,7 +8,7 @@ app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, "0.0.0.0", () => {
   console.log('app listening on', port);
